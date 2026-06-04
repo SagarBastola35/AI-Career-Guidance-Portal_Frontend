b3ffd7ef2e0f88fdbac700b9bd4712cc6f8bc038
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-06-04 14:56:55 +0530

    Update Navbar.jsx

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,4 +1,4 @@
-// frontend/src/components/Layout/Navbar.jsx
+
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
