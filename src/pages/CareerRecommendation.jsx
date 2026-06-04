c7cc884ef6510ca283813e979d43530adf9a8de5
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-06-04 14:58:18 +0530

    Update CareerRecommendation.jsx

--- src/pages/CareerRecommendation.jsx
+++ src/pages/CareerRecommendation.jsx
@@ -1,5 +1,4 @@
 
-// frontend/src/pages/CareerRecommendation.jsx
 import React, { useState, useEffect } from "react";
 import axios from '../utils/axios.js';
 import { motion } from "framer-motion";
