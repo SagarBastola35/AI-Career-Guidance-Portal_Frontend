fd987f270ae73912086c42d8d33456dad44a812b
SagarBastola35 <sagarbastolasagar3@gmail.com>
2026-06-04 14:59:49 +0530

    Remove commented path from Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-// frontend/src/pages/Register.jsx
+
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
